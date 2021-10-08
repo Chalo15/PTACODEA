d378ba297dedbb5ba6df12e9bb3d09aa96548e9b
Billy <bichacon98s@gmail.com>
2021-10-08 17:20:29 -0600

    Solucion de la ruta del logo de codea

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,32 +2,41 @@
 
 @section('content')
 <div class="container">
-    <div class="jumbotron">
-        <div class="">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-6 col-lg-6 ">
             <h1 class="display-5 text-center">Menu Principal</h1>
         </div>
     </div>
     <div class="row justify-content-center">
         <div class="col-sm-12 col-md-6 col-lg-6 text-center aligne-item-center">
-            <h3>Mi Perfil</h3>
-            <img class="rounded mx-auto d-block m-2" src="https://cdn-icons-png.flaticon.com/512/1361/1361728.png" alt="" width="150vh">
-            <button class="btn btn-primary">Ingresar</button>
+            <div class="mi_perfil">
+                <h3>Mi Perfil</h3>
+                <i class=" d-block fas fa-user p-2"></i>
+                <button class="btn btn-primary">Ingresar</button>
+            </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-6 text-center aligne-item-center">
-            <h3>Gimnasio</h3>
-            <img class="rounded mx-auto d-block m-2" src="https://cdn-icons-png.flaticon.com/512/69/69840.png" alt="" width="150vw">
-            <button class="btn btn-primary">Ingresar</button>
+            <div class="gimnasio">
+                <h3>Gimnasio</h3>
+                <i class=" d-block fas fa-dumbbell p-2"></i>
+                <button class="btn btn-primary">Ingresar</button>
+            </div>
         </div>
     </div>
     <div class="row justify-content-center">
         <div class="col-sm-12 col-md-6 col-lg-6 text-center aligne-item-center">
-            <h3>Registro de la Disciplina Deportiva</h3>
-            <button class="btn btn-primary">Ingresar</button>
+            <div class="atleta_registro">
+                <h3>Registro de la Disciplina Deportiva</h3>
+                <i class="d-block fas fa-running p-2"></i>
+                <button class="btn btn-primary">Ingresar</button>
+            </div>
         </div>
         <div class="col-sm-12 col-md-6 col-lg-6 text-center aligne-item-center">
-            <h3>Clinica de Fisioterapia</h3>
-            <i class="d-block fas fa-user-md"></i>
-            <button class="btn btn-primary">Ingresar</button>
+            <div class="fisioterapia_clinica">
+                <h3>Clinica de Fisioterapia</h3>
+                <i class="d-block fas fa-user-md p-2"></i>
+                <button class="btn btn-primary">Ingresar</button>
+            </div>
 
         </div>
     </div>
