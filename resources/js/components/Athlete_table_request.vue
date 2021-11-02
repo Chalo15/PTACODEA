2565c6c83b4cb231db301831793eb3fa766f0b35
sergioalfaro05 <49499947+sergioalfaro05@users.noreply.github.com>
2021-11-01 23:08:34 -0600

    controllerrequest

---
+++ resources/js/components/Athlete_table_request.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1 class "text-center">Solicitudes de Registro de Atletas</h1>
+        <hr>
+        <table class="table">
+        <thead class="thead-dark">
+            <tr>
+            <th scope="col">Numero de soliciutd</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellidos</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Id Categoría</th>
+            <th scope="col">Estado</th>
+            </tr>
+        </thead>
+        <tbody>        
+            <tr v-for = "Athlete_table_request in solicitudes" : key = "solicitud.id">
+            <th scope="row">{{ solicitud.id }}</th>
+            <td>{{ solicitud.name }}</td>
+            <td>{{ solicitud.last_name }}</td>
+            <td>{{ solicitud.email }}</td>
+            <td>{{ solicitud.sport_id }}</td>
+            <td>{{ solicitud.state }}</td>  
+            </tr>            
+        </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default{
+        Data(){
+            return {
+                solicitudes:[],
+            }
+        }
+        methods:{
+            async list(){
+                const res = await axios.get('solicitudes');
+                this.solicitudes = res.data;
+            }
+        },
+        created(){
+            this.list();
+        }
+    }
+</script>
+
+<style>
+
+</style>
