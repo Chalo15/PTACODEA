119898935498cebd27f4aadaa9b75614c48e54f0
PabloLizano <pablo99licua@gmail.com>
2021-11-03 12:32:37 -0600

    Creación de Vista de atletas por disciplina.
    Creación de la vista de atletas por disciplina impartida por el instrctor que consulte.

--- app/Http/Controllers/AthleteController.php
+++ app/Http/Controllers/AthleteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sport;
+use Illuminate\Support\Facades\Auth;
 
 class AthleteController extends Controller
 {
@@ -10,5 +11,23 @@ class AthleteController extends Controller
         return view('users.athletes',[
             'sports'=>Sport::all()
         ]);
+    }
+
+    function a_p_d(){
+        /*$data = User::select('users.name', 'users.lastname')
+                ->join('athletes', 'users.id', '=', 'athletes.user_id')
+                ->where("athletes.state", "=", 'a')
+                ->get();*/
+
+        Auth::loginUsingId(2);
+        $disciplina = Auth::user()->coach->sport;
+        $atletas = $disciplina->athletes;
+        $verif = $atletas->where("state", "=", 'a');
+        $users = $verif->map->user->flatten();
+        Auth::logout();
+
+
+        return view('users.athletesregis',['athletes'=>$users]);
+
     }
 }
