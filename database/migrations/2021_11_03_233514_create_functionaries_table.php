9e2d0ec39fd7421df1aa497fb7ee5d3d83f2910f
JosuePerez <83853281+rafinhaperez24@users.noreply.github.com>
2021-11-04 00:02:02 -0600

    Migration Func

---
+++ database/migrations/2021_11_03_233514_create_functionaries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFunctionariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('functionaries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('functionaries');
+    }
+}
