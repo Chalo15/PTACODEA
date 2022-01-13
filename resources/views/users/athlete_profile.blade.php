6f6888e25c823160f1a1e6c849d4aa1ef3370fe3
Billy <bichacon98s@gmail.com>
2022-01-13 00:07:13 -0600

    Porfile atleta y css

--- resources/views/users/athlete_profile.blade.php
+++ resources/views/users/athlete_profile.blade.php
@@ -2,121 +2,101 @@
 
 
 @section('content')
+<body class="body_porfile">
+    
     <div class="container">
+
         <div class="row justify-content-center">
+
             <div class="col-md-8">
                 <div class="card">
+                    <div class="card-header">
+                        <h3 class="d-5 text-center">Perfil Personal</h3>
+                    </div>
 
-                    <div class="text-center card-header"><h3 class="d-5">Perfil Personal</h3></div>
+                    <div class="card-body">
 
-                  
                         
-                     
-                       
                         <form class="well form-horizontal" action="{{route('saveProfile')}} " method="post"  id="formulario_perfil" enctype="multipart/form-data">
-                            
+                                
                             @csrf
                             @method('put')
-
-                            <!--foto de perfil-->
-
-                            <x-row>
-                                <div class="col-md-7">
-
-                                    <tr>
-                                        <img src="{{ asset('storage\imagenes\profile.png')}}"  width="25%">
-    
-                                    </tr>
-                                </div>
-                            </x-row>
-                            <br>
-
                             
-                                <div class="from-group row">
-                                    <div class="col-md-8 offset-md-4">
-                                        {{-- <button onclick="window.location='{{ route('changePhoto') }}'" class="btn btn-negro ml-auto m-1">Seleccionar foto de perfil</button> --}}
-                                        <a href="{{ route('changePhoto') }}" type="button" class="btn btn-negro">{{ __('Seleccionar foto de perfil') }}</a>
-                                        
-                                    </div>
+                            
+                            <!--foto de perfil-->  
+                            <div class="form-group">
+
+                                <div class="col-md-12 col-12 justify-content-center text-center">
+                                    <img src="{{ asset('img/Degradado.png')}}"  width="50%">
                                 </div>
-                                <br>
 
-                           
+                            </div>
+                                
+                            <div class="form-group">
+                                <div class="col-12 col-sm-12 col-12 justify-content-center text-center">
+                                    <a href="{{ route('changePhoto') }}" type="button" class="btn btn-negro">{{ __('Seleccionar foto de perfil') }}</a>
+                                </div>
+                            </div>
+                                
+                                
+                            <div class="form-group row">
 
-
-                            <x-row>
-                                <div class="col-md-7">
-
+                                <div class="col-12 col-sm-6 col-md-6">
                                     <label >Nombre</label>
                                     <input value="{{$persona->name}}" type="text" name="name" class="form-control form-control-sm ">
-                                    {{-- <input class="form-control" value="{{ $persona->name }}" placeholder="" type="text" name="name" class="form-control form-control-sm " > --}}
-                                    </div>
-                                {{-- <x-input value="{{$persona->name}}" name="nombre" placeholder="" label="Nombre"/>   --}}
-                            </x-row>                                                    
-
-                            <x-row>
-                                <div class="col-md-7">
-                                    <label >Apellidos</label>
-                                    <input value="{{$persona->lastname}}" type="text" name="lastname" class="form-control form-control-sm ">
                                 </div>
-                                {{-- <input class="form-control" value="{{ $persona->lastname }}" placeholder="" type="text" name="apellidos" class="form-control form-control-sm " > --}}
-                                {{-- <x-input value="{{$persona->lastname}}" name="apellidos" placeholder="" label="Apellidos"/>   --}}
-                            </x-row> 
-                            
-                            <x-row>
-                                <div class="col-md-7">
 
-                                    <label >Correo</label>
-                                  
-                                    <input value="{{$persona->email}}" type="text" name="email" class="form-control form-control-sm ">
+                                <div class="col-12 col-sm-6 col-md-6">
+                                        <label >Apellidos</label>
+                                        <input value="{{$persona->lastname}}" type="text" name="lastname" class="form-control form-control-sm ">
                                 </div>
                                 
-                                {{-- <input class="form-control" value="{{ $persona->email }}" placeholder="" type="text" name="correo" class="form-control form-control-sm " > --}}
-                                {{-- <x-input value="{{$persona->email}}" name="correo" placeholder="" label="Correo"/>   --}}
-                            </x-row>  
-                            <x-row>
-                                <div class="col-md-7">
+                            </div>
                                     
+                            <div class="form-group row">
+                                
+                                <div class="col-md-6">
+                                    <label >Correo</label>
+                                    <input value="{{$persona->email}}" type="text" name="email" class="form-control form-control-sm ">
+                                </div>
+                                <div class="col-md-6">
                                     <label >Fecha de nacimiento</label>
                                     <input value="{{$persona->birthdate}}" type="text" name="birthdate" class="form-control form-control-sm ">
                                 </div>
-                                {{-- <input class="form-control" value="{{ $persona->birthdate }}" placeholder="" type="text" name="nacimiento" class="form-control form-control-sm " > --}}
-                                {{-- <x-input value="{{$persona->birthdate}}" name="nacimiento" placeholder="" label="Fecha de nacimiento"/>   --}}
-                                </x-row> 
+                                
+                            </div>
                                 
                                 <div class="form-group row">
-                                    <div class="col-md-8 offset-md-4">
-                                        <x-row>
-                                            <div class="col-md-7">
-                                            
-                                                <label >Teléfono</label>
-                                                <input value="{{$persona->phone}}" type="text" name="phone" class="form-control form-control-sm ">
-                                            </div>
-
-                                        {{-- <input class="form-control" value="{{ $persona->phone }}" placeholder="" type="text" name="telefono" class="form-control form-control-sm " > --}}
-                                        {{-- <x-input value="{{$persona->phone}}" name="telefono" placeholder="" label="Teléfono"/>   --}}
-                                         </x-row>    
-                                         
-
-                            
-
-                            
-                            <div class="form-group row">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-negro m-1">
-                                        Actualizar datos
-                                    </button>
-                                    @can('role',"Atleta")
-                                    <a href="{{ route('datos_extra') }}" type="button" class="btn btn-negro">{{ __('Datos extra del atleta') }}</a>
-                                    @endcan
+                                    <div class="col-md-6">
+                                        <label >Teléfono</label>
+                                        <input value="{{$persona->phone}}" type="text" name="phone" class="form-control form-control-sm ">
+                                    </div>
+                                </div> 
+                                
+                                <div class="form-group row pt-2">
+                                    <div class="col-md-12 justify-content-center text-center">
+                                        <button type="submit" class="btn btn-negro m-1">
+                                            Actualizar datos
+                                        </button>
+                                        @can('role',"Atleta")
+                                        <a href="{{ route('datos_extra') }}" type="button" class="btn btn-negro">{{ __('Datos extra del atleta') }}</a>
+                                        @endcan
+                                    </div>
                                 </div>
-                            </div>
                             
+                            
+                        </form>
 
-                        </form> 
+                    </div>
 
-                 </div>
+                </div>
             </div>
+
         </div>
-    </div>      
+
+    </div>
+    
+</body>
+
 @endsection          
+                    
