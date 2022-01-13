0c512fd07cd37a85dcf27717d09d905136ff1ec7
PabloLizano <pablo99licua@gmail.com>
2022-01-12 21:14:00 -0600

    Vista Appointment
    Modificamos la vista appointment para cargar datos por default.

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::post('/users/athlete_extra_data',[App\Http\Controllers\ExtraDataControlle
 
 //Registrar datos de atleta por parte del fisioterapeuta
 Route::get('/physiotherapy/listAthletes',[App\Http\Controllers\FunctionaryController::class,'list'])->name('listAthletes')->middleware(['can:roles,"Fisioterapia"']);
-Route::get('/physiotherapy/appointment',[App\Http\Controllers\FunctionaryController::class,'appointment'])->name('appointment')->middleware(['can:roles,"Fisioterapia"']);
+Route::get('/physiotherapy/appointment/{id}',[App\Http\Controllers\FunctionaryController::class,'appointment'])->name('appointment')->middleware(['can:roles,"Fisioterapia"']);
 
 //Registrar datos de atleta por parte del encargado de musculacion
 Route::get('/musculation/report',[App\Http\Controllers\FunctionaryController::class,'report'])->name('report')->middleware(['can:roles,"Musculacion"']);
