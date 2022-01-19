43bdb5c480bfa1cf8ff59604f87e593aed856b39
Kevin González <kagn.521@gmail.com>
2022-01-19 16:45:03 -0600

    Cambiando layout de las vistas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,8 +86,26 @@
         @endcan
     </div>
 
+    @push('scripts')
+    <script>
+        console.log();
 
-    <div class="bodyHomePaige">
+    </script>
+    @endpush
+
+    @push('stylesheet')
+    <style>
+        .test-js {
+            background: red;
+        }
+
+    </style>
+    @endpush
+
+</x-app-layout>
+
+
+{{-- <div class="bodyHomePaige">
 
         <div class="homePaige container-fluid ">
 
@@ -108,232 +126,208 @@
                         <div class="atleta_registro">
                             <h4 class="">Analizar Solicitud</h4>
                             <i class="d-block fas fa-running p-2"></i>
-                            {{-- <a href="{{ route('requests.index') }}" class="btn btn-negro">
-                            Acceder
-                            </a> --}}
-                        </div>
-                    </div>
+                            <a href="{{ route('requests.index') }}" class="btn btn-negro">
+Acceder
+</a>
+</div>
+</div>
 
-                    <div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
-                        <div class="fisioterapia_clinica">
-                            <h4 class="">Fisioterapia</h4>
-                            <i class="d-block fas fa-user-md p-2"></i>
-                            <button class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
+<div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
+    <div class="fisioterapia_clinica">
+        <h4 class="">Fisioterapia</h4>
+        <i class="d-block fas fa-user-md p-2"></i>
+        <button class="btn btn-negro">Ingresar</button>
+    </div>
+</div>
 
-                    <div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
-                        <div class="gimnasio">
-                            <h4 class="">Gimnasio</h4>
-                            <i class=" d-block fas fa-dumbbell p-2"></i>
-                            <button class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
+<div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
+    <div class="gimnasio">
+        <h4 class="">Gimnasio</h4>
+        <i class=" d-block fas fa-dumbbell p-2"></i>
+        <button class="btn btn-negro">Ingresar</button>
+    </div>
+</div>
 
-                    <div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
-                        <div class="mi_perfil">
-                            <h4 class="">Mi Perfil</h4>
-                            <i class="d-block fas fa-user p-2"></i>
-                            <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
+<div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
+    <div class="mi_perfil">
+        <h4 class="">Mi Perfil</h4>
+        <i class="d-block fas fa-user p-2"></i>
+        <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
+    </div>
+</div>
 
-                    <div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
-                        <div class="Registrar_funcionario">
-                            <h4>Usuarios</h4>
-                            <i class="d-block fas fa-swimmer"></i>
-                            <a href="{{ route('users.index') }}" class="btn btn-negro">Acceder</a>
-                        </div>
-                    </div>
+<div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
+    <div class="Registrar_funcionario">
+        <h4>Usuarios</h4>
+        <i class="d-block fas fa-swimmer"></i>
+        <a href="{{ route('users.index') }}" class="btn btn-negro">Acceder</a>
+    </div>
+</div>
 
-                    <div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
-                        <div class="Registrar_funcionario">
-                            <h4>Deportes</h4>
-                            <a href="{{ route('sports.index') }}" class="btn btn-negro">Acceder</a>
-                        </div>
-                    </div>
+<div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
+    <div class="Registrar_funcionario">
+        <h4>Deportes</h4>
+        <a href="{{ route('sports.index') }}" class="btn btn-negro">Acceder</a>
+    </div>
+</div>
 
-                    <div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
-                        <div class="Registrar_funcionario">
-                            <h4>Atletas</h4>
-                            <a href="{{ route('athletes.index') }}" class="btn btn-negro">Acceder</a>
-                        </div>
-                    </div>
+<div class="col-12 col-sm-2 col-md-2 col-lg-2 text-center aligne-item-center">
+    <div class="Registrar_funcionario">
+        <h4>Atletas</h4>
+        <a href="{{ route('athletes.index') }}" class="btn btn-negro">Acceder</a>
+    </div>
+</div>
 
-                </div>
+</div>
 
+</div>
+@endcan
+
+
+@can('role',"Instructor")
+<div class="Instructor">
+
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-12 col-lg-12 ">
+            <h1 class="display-5 text-center py-5">Instructor</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center p-1">
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="mi_perfil">
+                <h4 class="">Mi Perfil</h4>
+                <i class="d-block fas fa-user p-2"></i>
+                <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
             </div>
-            @endcan
-
-
-            <!-- Vista del menu principal del Instructor -->
-
-            @can('role',"Instructor")
-            <div class="Instructor">
-
-                <div class="row justify-content-center">
-                    <div class="col-sm-12 col-md-12 col-lg-12 ">
-                        <h1 class="display-5 text-center py-5">Instructor</h1>
-                    </div>
-                </div>
-                <div class="row justify-content-center p-1">
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="mi_perfil">
-                            <h4 class="">Mi Perfil</h4>
-                            <i class="d-block fas fa-user p-2"></i>
-                            <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="registro_de_datos_atleta">
-                            <h4 class="">Registrar datos del atleta</h4>
-                            <i class="d-block fas fa-address-card p-2"></i>
-                            <a href="/users/instructor" class="btn btn-negro">Ingresar</a>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="reservar_gimnasio">
-                            <h4 class="">Reservar Instalaciones</h4>
-                            <i class=" d-block fas fa-book p-2"></i>
-                            <button onclick="window.location='{{ Route('booking_form') }}'" class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
-                </div>
-
+        </div>
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="registro_de_datos_atleta">
+                <h4 class="">Registrar datos del atleta</h4>
+                <i class="d-block fas fa-address-card p-2"></i>
+                <a href="/users/instructor" class="btn btn-negro">Ingresar</a>
             </div>
-
-            @endcan
-
-
-
-            <!-- Vista del menu principal del Atleta -->
-            @can('role',"Atleta")
-
-            <div class="Atleta">
-                <div class="row justify-content-center">
-                    <div class="col-sm-12 col-md-12 col-lg-12 ">
-                        <h1 class="main_text_atleta text-center py-5">Atleta</h1>
-                    </div>
-                </div>
-
-                <div class="row justify-content-center p-1">
-
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="reservar_gimnasio">
-                            <h4 class="">Reservar Instalaciones</h4>
-                            <i class=" d-block fas fa-book p-2"></i>
-                            <button onclick="window.location='{{ Route('booking_form') }}'" class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="mi_perfil">
-                            <h4 class="">Mi Perfil</h4>
-                            <i class="d-block fas fa-user-circle p-2"></i>
-                            <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="expediente">
-                            <h4 class="">Ver Expediente</h4>
-                            <i class="d-block fas fa-id-card-alt p-2"></i>
-                            <button class="btn btn-negro" onclick="window.location='{{ url('/athletes/seedata') }}'">Ingresar</button>
-                            {{-- falla --}}
-                        </div>
-                    </div>
-                </div>
-
+        </div>
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="reservar_gimnasio">
+                <h4 class="">Reservar Instalaciones</h4>
+                <i class=" d-block fas fa-book p-2"></i>
+                <button onclick="window.location='{{ Route('booking_form') }}'" class="btn btn-negro">Ingresar</button>
             </div>
-
-            @endcan
-
-
-
-            <!-- Vista del menu principal de Fisioterapia -->
-            @can('role',"Fisioterapia")
-
-            <div class="Fisioterapia">
-                <div class="row justify-content-center">
-                    <div class="col-sm-12 col-md-12 col-lg-12 ">
-                        <h2 class="display-5 text-center py-5">Fisioterapia</h2>
-                    </div>
-                </div>
-
-                <div class="row justify-content-center p-1">
-
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="mi_perfil">
-                            <h4 class="">Mi Perfil</h4>
-                            <i class="d-block fas fa-user-circle p-2"></i>
-                            <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="expediente">
-                            <h4 class="">Registrar datos de atleta</h4>
-                            <i class="d-block fas fa-id-card-alt p-2"></i>
-                            <button class="btn btn-negro" onclick="window.location='{{ url('/physiotherapy/listAthletes') }}'">Ingresar</button>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-
-            @endcan
-
-            <!-- Vista del menu principal de Musculacion -->
-            @can('role',"Musculacion")
-
-            <div class="Musculacion">
-                <div class="row justify-content-center">
-                    <div class="col-sm-12 col-md-12 col-lg-12 ">
-                        <h2 class="display-5 text-center py-5">Musculación</h2>
-                    </div>
-                </div>
-
-                <div class="row justify-content-center p-1">
-
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="mi_perfil">
-                            <h4 class="">Mi Perfil</h4>
-                            <i class="d-block fas fa-user-circle p-2"></i>
-                            <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
-                        <div class="expediente">
-                            <h4 class="">Registrar datos de atleta</h4>
-                            <i class="d-block fas fa-id-card-alt p-2"></i>
-                            <button class="btn btn-negro" onclick="window.location='{{ url('/musculation/catalogAthletes') }}'">Ingresar</button>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            @endcan
-
         </div>
     </div>
 
-    @push('scripts')
-    <script>
-        console.log();
+</div>
 
-    </script>
-    @endpush
+@endcan
 
-    @push('stylesheet')
-    <style>
-        .test-js {
-            background: red;
-        }
 
-    </style>
-    @endpush
 
-</x-app-layout>
+@can('role',"Atleta")
+
+<div class="Atleta">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-12 col-lg-12 ">
+            <h1 class="main_text_atleta text-center py-5">Atleta</h1>
+        </div>
+    </div>
+
+    <div class="row justify-content-center p-1">
+
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="reservar_gimnasio">
+                <h4 class="">Reservar Instalaciones</h4>
+                <i class=" d-block fas fa-book p-2"></i>
+                <button onclick="window.location='{{ Route('booking_form') }}'" class="btn btn-negro">Ingresar</button>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="mi_perfil">
+                <h4 class="">Mi Perfil</h4>
+                <i class="d-block fas fa-user-circle p-2"></i>
+                <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="expediente">
+                <h4 class="">Ver Expediente</h4>
+                <i class="d-block fas fa-id-card-alt p-2"></i>
+                <button class="btn btn-negro" onclick="window.location='{{ url('/athletes/seedata') }}'">Ingresar</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endcan
+
+
+
+@can('role',"Fisioterapia")
+
+<div class="Fisioterapia">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-12 col-lg-12 ">
+            <h2 class="display-5 text-center py-5">Fisioterapia</h2>
+        </div>
+    </div>
+
+    <div class="row justify-content-center p-1">
+
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="mi_perfil">
+                <h4 class="">Mi Perfil</h4>
+                <i class="d-block fas fa-user-circle p-2"></i>
+                <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="expediente">
+                <h4 class="">Registrar datos de atleta</h4>
+                <i class="d-block fas fa-id-card-alt p-2"></i>
+                <button class="btn btn-negro" onclick="window.location='{{ url('/physiotherapy/listAthletes') }}'">Ingresar</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+@endcan
+
+@can('role',"Musculacion")
+
+<div class="Musculacion">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-12 col-lg-12 ">
+            <h2 class="display-5 text-center py-5">Musculación</h2>
+        </div>
+    </div>
+
+    <div class="row justify-content-center p-1">
+
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="mi_perfil">
+                <h4 class="">Mi Perfil</h4>
+                <i class="d-block fas fa-user-circle p-2"></i>
+                <button onclick="window.location='{{ route('perfil.atleta') }}'" class="btn btn-negro">Ingresar</button>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center aligne-item-center">
+            <div class="expediente">
+                <h4 class="">Registrar datos de atleta</h4>
+                <i class="d-block fas fa-id-card-alt p-2"></i>
+                <button class="btn btn-negro" onclick="window.location='{{ url('/musculation/catalogAthletes') }}'">Ingresar</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endcan
+
+</div>
+</div> --}}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
 
 <head>
     <meta charset="utf-8">
@@ -15,27 +15,56 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        main>.container {
+            padding: 60px 15px 0;
+        }
+
+        .footer>.container {
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+
+        code {
+            font-size: 80%;
+        }
+
+    </style>
+
     @stack('stylesheet')
+
 </head>
 
-{{-- <body class="bg-dark"> --}}
-<body class="bg-white-codea">
-    <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm"> --}}
-        <nav class="navbar navbar-expand-md navbar-black-codea bg-black-codea shadow-sm">
+<body class="d-flex flex-column h-100">
+
+    <header>
+        <nav class="navbar navbar-expand-md navbar-black-codea fixed-top bg-black-codea">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('img/logo-codea.gif') }}" alt="" width="30%" height="30%">
+                    <img src="{{ asset('img/logo-codea.gif') }}" alt="" width="25%" height="25%">
                 </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="main-navbar">
-
+                <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav ml-auto">
-
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->full_name }}
@@ -60,18 +89,28 @@
 
                             </div>
                         </li>
-
                     </ul>
                 </div>
             </div>
         </nav>
+    </header>
 
-        <main class="container my-5">
-
+    <main role="main" class="flex-shrink-0 mt-5">
+        <div class="container my-5">
             {{ $slot }}
+        </div>
+    </main>
 
-        </main>
-    </div>
+    <footer class="footer mt-auto py-3 bg-black-codea">
+        <div class="container text-white-codea text-muted">
+
+            <div class="float-right d-none d-sm-inline">
+                {{ 'v' . config('app.version') }}
+            </div>
+
+            <strong>Copyright &copy; 2022 <a href="{{ config('app.url') }}">{{ config('app.name') }}</a>.</strong> All rights reserved.
+        </div>
+    </footer>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
