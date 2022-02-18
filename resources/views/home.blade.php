e5a17d0f26f94770234e634438f2f6517310bfcc
JosuePerez <jdps0497@gmail.com>
2022-02-17 22:20:44 -0600

    Varios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -127,7 +127,7 @@
                     <i class="fas fa-calendar-week fa-5x"></i>
                 </div>
                 <div class="card-footer">
-                    <a href="{{ route('events.index') }}" class="btn btn-primary btn-block">Acceder &nbsp;
+                    <a href="#" class="btn btn-primary btn-block">Acceder &nbsp;
                         <i class="fas fa-share"></i></a>
                 </div>
             </div>
