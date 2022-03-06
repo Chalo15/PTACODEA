cc617a17f0f5dfc4f37b2e4324461d6c98d2fc78
Billy <bichacon98s@gmail.com>
2022-03-05 19:45:13 -0600

    Actualizando notificaciones

--- resources/views/layouts/partials/_notifications.blade.php
+++ resources/views/layouts/partials/_notifications.blade.php
@@ -14,11 +14,15 @@
         axios.put('/notifications/markAll').then(() => this.refresh());
     },
     message(notification) {
-        if (this.role == 6) {
-            return `El atleta ${notification.data.Athlete_name} ${notification.data.Athlete_last_name} reservó una cita.`;
+        if (this.role == 6 || this.role == 5) {
+            return `El atleta ${notification.data.Nombre_Atleta} ${notification.data.Apellidos_Atleta}, cédula ${notification.data.Id_Atleta} reservó una cita   `;
 
         } else if (this.role == 4) {
-            return `${notification.data.Muscular_name} aceptó la reserva.`;
+            if(notification.data.State == 'CONFIRMADA'){
+                return `${notification.data.Nombre_Musculacion} aceptó la reserva.`;
+            }else if(notificationdata.State == 'PENDIENTE'){
+                return `${notification.data.Nombre_Musculacion} rechazó la reserva.`;
+            }
         }
     },
     deny() {
@@ -26,8 +30,8 @@
             this.refresh();
         });
     },
-    accept() {
-        axios.put('/notifications/markAll').then((respuesta) => {
+    accept(appointment) {
+        axios.put(`/notfifications/${appointment}/accept`).then((respuesta) => {
             this.refresh();
         });
     },
@@ -37,7 +41,7 @@
 }" x-init="init()">
 
     <a id="notifications-dropdown" class="mt-1 ml-2 nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-        <i class="fas fa-solid fa-bell"></i> Notificaciones
+        <i class="fas fa-envelope"></i> Notificaciones
         <span x-show="notifications.length > 0" x-text="notifications.length" class="badge badge-pill badge-info"></span>
     </a>
 
@@ -45,7 +49,7 @@
 
         <span x-show="notifications.length > 0">
             <button class="dropdown-item text-center" type="button" x-on:click="readAll()">
-                Marcar todo como leido
+                <i class="fas fa-envelope-open"></i> Marcar todo como leido
             </button>
 
             <div class="dropdown-divider"></div>
@@ -59,43 +63,43 @@
             <div class="dropdown-item">
 
                 <span class="d-inline">
-                    <<<<<<< HEAD El atleta <span x-text="notification.data.Nombre_Atleta">
-                </span>
-                <span x-text="notification.data.Apellidos_Atleta"></span>
-                reservó una cita
+                    <span x-text="message(notification)"></span> &nbsp;
+                    @if(auth()->user()->role_id == 4)
+                    <div x-show="role == 4" class="d-inline">
+                        <button type="button" class="d-inline btn" x-on:click="read(notification.id)">
+                            <i class="fas fa-envelope-open" data-toggle="tooltip" data-placement="top" title="Marcar como leido"></i>
+                        </button>
+                    </div>
+                    @elseif(auth()->user()->role_id == 6 || auth()->user()->role_id == 5 )
+                    <div x-show="role == 6" class="d-inline">
+                        <button type="button" class="d-inline btn" x-on:click="read(notification.id)">
+                            <i class="fas fa-envelope-open" data-toggle="tooltip" data-placement="top" title="Marcar como leido"></i>
+                        </button>
+                        <button type="button" class="d-inline btn" x-on:click="accept(notification.id)">
+                            <i data-toggle="tooltip" data-placement="top" title="Aceptar Reserva" class="fas fa-check-circle"></i>
+                        </button>
+                        <button type="button" class="d-inline btn" x-on:click="read(notification.id)">
+                            <i class="fas fa-ban" data-toggle="tooltip" data-placement="top" title="Denegar Reserva"></i>
+                        </button>
+                    </div>
+                    @endif
 
-                <button type="button" class="btn" x-on:click="read(notification.id)">
-                    <i class="far fa-envelope-open" data-toggle="tooltip" data-placement="top" title="Marcar como leido"></i>
-                </button>
-                </span>
-
-                <div class="dropdown-divider"></div>
-
-                @elseif(auth()->user()->role_id == 4)
-
-                <span x-text="notification.data.Muscular_name"></span> aceptó la reserva
-                <button type="button" class="d-inline btn" x-on:click="read(notification.id)"><i data-toggle="tooltip" data-placement="top" title="Marcar como leido" class="fas fa-check-circle"></i></button>
-
-                @endif
-
-                </li>
-                =======
-                <span x-text="message(notification)"></span> &nbsp;
-
-                <span role="button" x-on:click="read(notification.id)">
-                    <i class="fas fa-check-circle" data-toggle="tooltip" data-placement="top" title="Marcar como leido"></i>
-                </span>
                 </span>
 
             </div>
-            >>>>>>> aa1f14e67460bfc2b3404b2f85bfbf58b58782e6
         </template>
 
         <div class="dropdown-divider"></div>
 
-        <a href="{{ route('notifications.index') }}" class="dropdown-item text-center">
+        <a href="{{ route('notifications.index') }}" class="my-1 dropdown-item text-center">
             Ver todo
         </a>
-
+        @can('role',['Musculacion','Fisioterapia'])
+        <div class="text-center">
+            <a href="{{ route('appointments.index') }}" class=" btn btn-primary">
+                <i class="mr-1 fas fa-calendar-check"></i> Ir a citas
+            </a>
+        </div>
+        @endcan
     </div>
 </div>
