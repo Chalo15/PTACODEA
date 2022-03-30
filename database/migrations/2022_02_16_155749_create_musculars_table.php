6e1487128181d72c8fe64ac189b5762d61140bdb
JosuePerez <jdps0497@gmail.com>
2022-03-29 20:17:34 -0600

    Update 2022_02_16_155749_create_musculars_table.php

--- database/migrations/2022_02_16_155749_create_musculars_table.php
+++ database/migrations/2022_02_16_155749_create_musculars_table.php
@@ -19,7 +19,6 @@ class CreateMuscularsTable extends Migration
             $table->foreignId('athlete_id')->constrained('athletes');
             $table->foreignId('appointment_id')->nullable()->constrained('appointments');
             $table->date('date');
-            $table->string('calcification');
             $table->float('physiological_age');
             $table->float('weight');
             $table->float('height');
@@ -37,13 +36,16 @@ class CreateMuscularsTable extends Migration
             $table->float('elbow');
             $table->float('knee');
             $table->float('biceps');
+            $table->float('calf_cm');
             $table->float('calories');
             $table->float('bmi_high');
             $table->float('icc_high');
             $table->float('fat');
             $table->float('residual');
             $table->float('bone');
+            $table->float('muscle');
             $table->float('visceral');
+            $table->float('ideal_weight');
             $table->text('get_better');
             $table->longText('details');
             $table->timestamps();
