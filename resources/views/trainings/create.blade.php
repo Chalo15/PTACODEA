0e86f43495684c46a37696cbc8670a0d2a1e3bf3
JosuePerez <jdps0497@gmail.com>
2022-05-16 22:41:28 -0600

    Trainings validations

--- resources/views/trainings/create.blade.php
+++ resources/views/trainings/create.blade.php
@@ -17,7 +17,7 @@
                 </div>
 
                 <div class="card-body">
-                    <form action="{{ route('trainings.store') }}" method="POST">
+                    <form id="form_create_trainings" action="{{ route('trainings.store') }}" method="POST">
                         @csrf
 
                         {{-- Atleta --}}
@@ -28,9 +28,10 @@
                                     <option disabled {{ old('athlete_id') ? '' : 'selected' }} value=""> -- Seleccione
                                         -- </option>
                                     @foreach ($athletes as $athlete)
-                                    <option {{ old('athlete_id') == $athlete->id ? 'selected' : '' }} value="{{ $athlete->id }}">
-                                        {{ $athlete->user->identification . ' | ' . $athlete->user->name . ' ' . $athlete->user->last_name }}
-                                    </option>
+                                        <option {{ old('athlete_id') == $athlete->id ? 'selected' : '' }}
+                                            value="{{ $athlete->id }}">
+                                            {{ $athlete->user->identification . ' | ' . $athlete->user->name . ' ' . $athlete->user->last_name }}
+                                        </option>
                                     @endforeach
                                 </x-select2>
                             </div>
@@ -38,16 +39,21 @@
 
                         {{-- Fecha de registro --}}
                         @php
-                        $today = today()->toDateString();
-                        $lastWeek = today()->subDays(7)->toDateString();
-                        $nextWeek = today()->addDay(7)->toDateString();
+                            $today = today()->toDateString();
+                            $lastWeek = today()
+                                ->subDays(7)
+                                ->toDateString();
+                            $nextWeek = today()
+                                ->addDay(7)
+                                ->toDateString();
                         @endphp
 
                         <div class="form-group row">
 
                             <label for="date" class="col-sm-4 col-form-label">Fecha</label>
                             <div class="col-sm-8">
-                                <x-input name="date" type="date" min="{{ $lastWeek }}" max="{{ $nextWeek }}" value="{{ $today }}" />
+                                <x-input name="date" type="date" min="{{ $lastWeek }}" max="{{ $nextWeek }}"
+                                    value="{{ $today }}" />
                             </div>
                         </div>
 
@@ -125,6 +131,75 @@
             </div>
         </div>
     </div>
+    @push('scripts')
+        <script>
+            $(document).ready(function() {
+                //Método que valida solo numeros
+                jQuery.validator.addMethod("numbersonly", function(value, element) {
+                    return this.optional(element) || /^[0-9,":"]+$/i.test(value);
+                }, 'Por favor digite solo valores numéricos y números naturales *', );
+                //Validaciones del formulario
+                if ($("#form_create_trainings").length > 0) {
+                    $('#form_create_trainings').validate({
+                        rules: {
+                            date: {
+                                required: true
+                            },
+                            type_training: {
+                                required: true
+                            },
+                            calification: {
+                                required: true
+                            },
+                            time: {
+                                required: true,
+                                numbersonly: true
+                            },
+                            level: {
+                                required: true
+                            },
+                            get_better: {
+                                required: true
+                            },
+                            planification: {
+                                required: true
+                            },
+                            details: {
+                                required: true
+                            },
+                        },
+
+                        messages: {
+                            date: {
+                                required: 'Por favor seleccione una fecha *'
+                            },
+                            type_training: {
+                                required: 'Por favor ingrese el tipo de entrenamiento *'
+                            },
+                            calification: {
+                                required: 'Por favor ingrese la calificación *'
+                            },
+                            time: {
+                                required: 'Por favor la duración del entrenamiento *'
+                            },
+                            level: {
+                                required: 'Por favor ingrese el nivel *'
+                            },
+                            get_better: {
+                                required: 'Por favor ingrese los aspectos a mejorar *'
+                            },
+                            planification: {
+                                required: 'Por favor ingrese la planificación *'
+                            },
+                            lesion: {
+                                required: 'Por favor ingrese la lesión que presenta *'
+                            },
+                        }
+                    });
+                }
+            });
+        </script>
+    @endpush
 
 
 </x-app-layout>
